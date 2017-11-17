9e310b97348d59d88386799bc77757a5d1337289
Movva <movvaragini@gmail.com>
2017-11-16 20:20:50 -0500

    testing push

--- CharacterInput_Ex1.py
+++ CharacterInput_Ex1.py
@@ -4,7 +4,8 @@ Create a program that asks the user to enter their name and their age. Print out
 Extras:
 (1) Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. (Hint: order of operations exists in Python)
 (2) Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button)
-Adding simple comment'''
+Adding simple comment
+Adding onemore comment'''
 from datetime import date
 def main():
 	name= input("Enter name:")
